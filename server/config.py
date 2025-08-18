0561b64c524b8b949893c564951384b6fe7ca273
Achors <adolfnyerere@gmail.com>
2025-08-19 01:19:29 +0200

    1. Added production urls 2[next]

--- server/config.py
+++ server/config.py
@@ -4,10 +4,14 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///nerakcos.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///nerakcos.db')
+    PORT = 10000
 
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///nerakcos.db')
+    PORT = os.environ.get('PORT', 5000)
